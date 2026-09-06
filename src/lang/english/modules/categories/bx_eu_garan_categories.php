8c461298b335912abba874fddd9f3025ee2b2ea0
Axel Benkert <31624452+bx-codemaster@users.noreply.github.com>
2026-09-06 12:44:08 +0200

    Update category hook description to warranty only

--- src/lang/english/modules/categories/bx_eu_garan_categories.php
+++ src/lang/english/modules/categories/bx_eu_garan_categories.php
@@ -4,4 +4,4 @@
    ---------------------------------------------------------------------------------------*/
 
 define('MODULE_CATEGORIES_BX_EU_GARAN_CATEGORIES_TITLE', 'BX EU Garan product hooks');
-define('MODULE_CATEGORIES_BX_EU_GARAN_CATEGORIES_DESCRIPTION', 'Stores EU warranty and repairability data when creating and editing products.');
+define('MODULE_CATEGORIES_BX_EU_GARAN_CATEGORIES_DESCRIPTION', 'Stores EU warranty data when creating and editing products.');
